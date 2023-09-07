0bb2f1fe5e2d16bb85bc0b491f1998cc9e57e2de
Andrey Marakulin <annndruha.github@gmail.com>
2023-09-07 14:23:01 +0300

    Fix and better logs

--- src/handlers.py
+++ src/handlers.py
@@ -10,6 +10,7 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ContextTypes, CallbackContext
 
 from src.settings import Settings
+from log_formatter import log_formatter
 
 settings = Settings()
 
@@ -25,11 +26,11 @@ def error_handler(func):
                 await func(update, context)
             except RequestException as err:
                 logging.error(err.__class__.__name__)
-                await context.bot.send_message(chat_id=update.message.chat_id, text=str(err.__class__.__name__))
+                await context.bot.send_message(chat_id=update.effective_user.id, text=str(err.__class__.__name__))
             except Exception as err:
                 logging.error(err)
                 traceback.print_tb(err.__traceback__)
-                await context.bot.send_message(chat_id=update.message.chat_id, text=str(err.__class__.__name__))
+                await context.bot.send_message(chat_id=update.effective_user.id, text=str(err.__class__.__name__))
         except Exception as err:
             logging.error("Fail to send error message:", err)
             traceback.print_tb(err.__traceback__)
@@ -38,79 +39,74 @@ def error_handler(func):
 
 
 @error_handler
+@log_formatter
 async def handler_start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = """🤖 Hi from relay bot!\n\nAvailable commands:\n/switch - Change state\n/turn_on - Turn lamp on\n""" \
            """/turn_off - Turn lamp off\nYou can easy find this commands in 'Menu'"""
     await context.bot.send_message(chat_id=update.message.chat_id, text=text)
-    logging.info(f'[{update.message.from_user.id} {update.message.from_user.full_name}] call /start')
 
 
 @error_handler
+@log_formatter
 async def handler_help(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = """This bot created for removed lamp control.\n\nAvailable commands:\n/switch - Change state\n""" \
            """/turn_on - Turn lamp on\n/turn_off - Turn lamp off\nYou can easy find this commands in 'Menu'""" \
            """\nTechnical commands:\n/get_state - Get current state\n/uptime - ESP uptime"""
     await context.bot.send_message(chat_id=update.message.chat_id, text=text)
-    logging.info(f'[{update.message.from_user.id} {update.message.from_user.full_name}] call /help')
 
 
 @error_handler
+@log_formatter
 async def handler_button(update: Update, context: CallbackContext) -> None:
-    logging.info(f'[{update.callback_query.from_user.id} {update.callback_query.from_user.full_name}]'
-                 f'[{update.callback_query.message.id}] button pressed with callback_data={update.callback_query.data}')
-    callback_data = update.callback_query.data
-
-    if callback_data == 'switch':
+    if update.callback_query.data == 'switch':
         r = requests.get(settings.RELAY_URL + "/switch", timeout=3)
         text = r.text + '\n' + str(datetime.datetime.now())
         keyboard = InlineKeyboardMarkup([[InlineKeyboardButton('Switch state', callback_data='switch')]])
     else:
         text = 'Видимо бот обновился, эта кнопка больше недоступна.'
-        keyboard = InlineKeyboardMarkup([[InlineKeyboardButton('Switch state', callback_data='switch')]])
+        keyboard = None
         logging.warning(f'[{update.callback_query.from_user.id} {update.callback_query.from_user.full_name}]'
-                        f'[{update.callback_query.message.id}] button pressed with old callback_data={callback_data}')
+                        f'[old callback {update.callback_query.message.id}]: {update.callback_query.data}')
     await update.callback_query.edit_message_text(text=text, reply_markup=keyboard)
 
 
 @error_handler
+@log_formatter
 async def handler_message(update: Update, context: CallbackContext) -> None:
-    logging.info(f'[{update.message.from_user.id} {update.message.from_user.full_name}] send message:'
-                 f'{repr(update.message.text)}')
-    text = 'Button below!'
     keyboard = InlineKeyboardMarkup([[InlineKeyboardButton('Switch state', callback_data='switch')]])
-    await context.bot.send_message(chat_id=update.message.chat_id, text=text, reply_markup=keyboard)
+    await context.bot.send_message(chat_id=update.message.chat_id, text='Button below!', reply_markup=keyboard)
 
 
 @error_handler
+@log_formatter
 async def handler_switch(update: Update, context: CallbackContext) -> None:
-    logging.info(f'[{update.message.from_user.id} {update.message.from_user.full_name}] call /switch')
     r = requests.get(settings.RELAY_URL + "/switch", timeout=3)
     await context.bot.send_message(chat_id=update.message.chat_id, text=r.text)
 
 
 @error_handler
+@log_formatter
 async def handler_turn_on(update: Update, context: CallbackContext) -> None:
-    logging.info(f'[{update.message.from_user.id} {update.message.from_user.full_name}] call /turn_on')
     r = requests.get(settings.RELAY_URL + "/set?value=1", timeout=3)
     await context.bot.send_message(chat_id=update.message.chat_id, text=r.text)
 
 
 @error_handler
+@log_formatter
 async def handler_turn_off(update: Update, context: CallbackContext) -> None:
-    logging.info(f'[{update.message.from_user.id} {update.message.from_user.full_name}] call /turn_off')
     r = requests.get(settings.RELAY_URL + "/set?value=0", timeout=3)
     await context.bot.send_message(chat_id=update.message.chat_id, text=r.text)
 
 
 @error_handler
+@log_formatter
 async def handler_uptime(update: Update, context: CallbackContext) -> None:
-    logging.info(f'[{update.message.from_user.id} {update.message.from_user.full_name}] call /uptime')
     r = requests.get(settings.RELAY_URL + "/uptime", timeout=3)
     await context.bot.send_message(chat_id=update.message.chat_id, text=r.text)
 
 
 @error_handler
+@log_formatter
 async def handler_get_state(update: Update, context: CallbackContext) -> None:
-    logging.info(f'[{update.message.from_user.id} {update.message.from_user.full_name}] call /get_state')
     r = requests.get(settings.RELAY_URL + "/get", timeout=3)
     await context.bot.send_message(chat_id=update.message.chat_id, text=r.text)
---
+++ src/log_formatter.py
@@ -0,0 +1,26 @@
+import functools
+import logging
+
+from telegram import Update
+from telegram.ext import ContextTypes
+
+
+def log_formatter(func):
+    @functools.wraps(func)
+    async def wrapper(update: Update, context: ContextTypes.DEFAULT_TYPE):
+        actor_handler = f'[{update.effective_user.id} {update.effective_user.full_name}] [{func.__name__}]'
+        if update.callback_query is not None:
+            logging.info(f'{actor_handler} [callback {update.callback_query.message.id}]: {update.callback_query.data}')
+        elif update.message is not None:
+            if update.message.text is not None:
+                logging.info(f'{actor_handler} [text]: {repr(update.message.text)}')
+            elif update.message.caption is not None:
+                logging.info(f'{actor_handler} [caption]: {repr(update.message.caption)}')
+            else:
+                logging.info(f'{actor_handler} [UNKNOWN MESSAGE TYPE]')
+        else:
+            logging.info(f'{actor_handler} [UNKNOWN UPDATE TYPE]')
+
+        await func(update, context)
+
+    return wrapper
